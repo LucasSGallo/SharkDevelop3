647c623155fd440389722ec3c36be983a677a27c
Melisa Riera <112903919+meliriera@users.noreply.github.com>
2023-05-06 12:17:11 -0300

    Leccion 04 Java (parte1)
    Solo sobreescritura de métodos Overriding y Ejercicio

---
+++ Java3/Leccion04/Sobreescritura/src/Test/TestSobreescritura.java
@@ -0,0 +1,14 @@
+
+package Test;
+
+import domain.Gerente;
+
+public class TestSobreescritura {
+    public static void main (String[] args) {
+        Gerente gerente1 = new Gerente("Jose ", 5000, "Sistemas");
+        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+    }
+  
+           
+    
+}
