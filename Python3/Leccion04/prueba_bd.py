3045a55943d7d9b01b05e63c4cf9fd5cb083da85
AleCarb <alecarb36@gmail.com>
2023-05-21 19:49:11 -0300

    done!

--- Python3/Leccion04/prueba_bd.py
+++ Python3/Leccion04/prueba_bd.py
@@ -1,21 +1,18 @@
 import psycopg2
-conexion = psycopg2.connect(
-    user='postgres',
-    password='*****',
-    host=127.0.0.1, #'localhost'
-    port=5432,
-    dadabase='test_db'
-)
+
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
 
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            cursor = conexion.cursor()
-            cursor.execute('SELECT * FROM tb_persona WHERE id_persona = %s',input('Digite el id que necesite')) #place holder (marcador de lugar)
-            registros = cursor.fetchone()
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s' #Placeholder
+            id_persona = input("Digite un nro para el id_persona: ")
+            cursor.execute(sentencia, (id_persona, ))  # De esta manera ejecutamos la sentencia
+            registros = cursor.fetchone()  # Recuperamos todos los registros que seran una lista
             print(registros)
 except Exception as e:
-        print(f"Ocurrió un error: {e}")
+    print(f"Ocurrió un error: {e}")
 finally:
-        #cursor.close() esto ya lo hace el with
-        conexion.close() # pero esto no
+    conexion.close()
+
+# https://www.psycopg.org/docs/usage.html
